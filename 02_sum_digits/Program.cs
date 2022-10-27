b42e1790061900657c04c536bcf115dce35fd588
Anna <ann.sxd@example.com>
2022-10-27 09:42:23 +0300

    2 задача-сумма цифр в числе

---
+++ 02_sum_digits/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт 
+// сумму цифр в числе.
+
+int GetSumOfDigit(int n)
+{
+    int sum = 0;
+    int digit = 0;
+    do
+    {
+        digit = n % 10;
+        sum += digit;
+        n /= 10;
+    } while (n > 0);
+    return sum;
+}
+
+Console.Clear();
+Console.Write("Введите число ");
+int number = int.Parse(Console.ReadLine() ?? "0");
+
+Console.WriteLine(GetSumOfDigit(number));
