f10e22e1c32aaeb16cc95a6abfd394b4e2959c99
Anna <ann.sxd@example.com>
2022-10-27 10:30:04 +0300

    3 задача на массив

---
+++ 03_array/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая задаёт массив из n элементов и выводит их на экран.
+
+
+void RandomArray(int n)
+{
+    int[] array = new int[n];
+    Random rnd = new Random();
+    for (int i = 0; i < n; i++)
+    {
+        array[i] = rnd.Next(0, 20);
+        Console.Write($"{array[i]} ");
+    }
+}
+
+Console.Clear();
+Console.Write("Введите длину массива: ");
+int arrayLength = int.Parse(Console.ReadLine() ?? "0");
+
+RandomArray(arrayLength);
+
